c4d6128aeff2743210fd4b5ea01d46ccf906319b
szele <szele@freemail.hu>
2020-10-08 10:02:51 +0200

    202010081002

--- HtmlBuilder/src/main/java/HtmlBuilder/ButtonElement.java
+++ HtmlBuilder/src/main/java/HtmlBuilder/ButtonElement.java
@@ -1,15 +1,12 @@
 package HtmlBuilder;
 
-public class ButtonElement extends ContainerElement {
-
+public class ButtonElement extends BasicHtmlElement { // ugye Buttonnak nincs tartalma, nem container
     public ButtonElement() {
-
         name = "button";
-
     }
-    public void setId(String id) { super.setProperty(id); }
+    public void setId(String id) { super.setProperty("id",id); }
 
     public void setType(String type) { super.setType(type); }
 
-    public void onClick(String whatToDo) {}
+    public void onClick(String whatToDo) {  super.setProperty("onClick", whatToDo);  }
 }
